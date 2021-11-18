cafe89a73968472259eaf8accbe5968d0c60eee0
alexmsf <xana.ferreira2000@hotmail.com>
2021-11-18 20:09:19 +0000

    added graphs

--- dm-proj1/graphs.r
+++ dm-proj1/graphs.r
@@ -30,25 +30,25 @@ df <- df %>%
 
 ggplot(loans, aes(x = status, y = amount)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to loan amount")
+  ggtitle("Loans' Results in function of the Requested Loan Amount")
 
 ggplot(loans, aes(x = status, y = payments)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to monthly pay")
+  ggtitle("Loans' Results in function of the Monthly Loan Payments")
 
 ggplot(loans, aes(x = status, y = duration)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to loan duration")
+  ggtitle("Loans' Results in function of the Requested Loan Duration")
 
 # Month impact on loan approval
 ggplot(loans, aes(x = month, group = status, fill = status)) +
   geom_bar(position = "fill") +
-  ggtitle("Month's impact on loans")
+  ggtitle("Month's impact on Loans")
 
 # Year impact on loan approval
 ggplot(loans, aes(x = year, group = status, fill = status)) +
   geom_bar(position = "fill") +
-  ggtitle("Year's impact on loans")
+  ggtitle("Year's impact on Loans")
 
 #####################################################
 
@@ -70,7 +70,7 @@ ggplot(df, aes(x = status, y = loan_duration)) +
 # Loan's Acceptance Rate in function of the client's average salary
 ggplot(df, aes(x = status, y = average_salary)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to client's district average salary")
+  ggtitle("Loans' Results in function of the Client's District's Average Salary")
 
 # Loan's Acceptance Rate in function of the client's region's crime rate in 96 
 ggplot(df, aes(x = status, y = crime96_S)) +
@@ -92,10 +92,10 @@ ggplot(df, aes(x = status, y = unemployment96)) +
   geom_boxplot() +
   ggtitle("Accepted loans according to client's district unemployment rate in 96")
 
-# Loan's Acceptance Rate in function of the client's region's nb of entrepeneurs per 1000 inhabitants 
+# Loan's Acceptance Rate in function of the client's region's nb of entrepreneurs per 1000 inhabitants 
 ggplot(df, aes(x = status, y = nb_enterpreneurs_per1000)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to client's district nb of entrepeneurs per 1000 inhabitants")
+  ggtitle("Loans' Results in function of the Client's District's Number of Entrepreneurs per 1000 inhabitants")
 
 # Loan's Acceptance Rate in function of the client's region's ratio urban inhabitants 
 ggplot(df, aes(x = status, y = ratio_urban_inhabitants)) +
@@ -115,18 +115,18 @@ ggplot(df, aes(x = status, y = nb_inhabitants)) +
 # Loan's Acceptance Rate in function of the client's age 
 ggplot(df, aes(x = status, y = age)) +
   geom_boxplot() +
-  ggtitle("Accepted loans according to client's age")
+  ggtitle("Loans' Results in function of the Client's Age")
 
 ####
 
 # Region's Impact on loan's success 
 ggplot(df, aes(x = region, group = status, fill = status)) +
   geom_bar(position = "fill") +
-  ggtitle("Region impact on loan's success")
+  ggtitle("Region's Impact on Loan's success")
 
 # Gender's Impact on loan's success 
 ggplot(df, aes(x = gender, group = status, fill = status)) +
-  geom_bar(position = "fill", stat = "percentage",) +
+  geom_bar(position = "fill") +
   ggtitle("Gender's impact on loan's success")
 
 # Account's Frequency Access Impact on loan's success 
@@ -135,13 +135,13 @@ ggplot(df, aes(x = account_freq_access, group = status, fill = status)) +
   ggtitle("Account's Frequency Access impact on loan's success")
 
 # Loan's Date Impact on loan's success 
-ggplot(df, aes(x = loan_date, group = status, fill = status)) +
+ggplot(df, aes(x = loan_date_month, group = status, fill = status)) +
   geom_bar(position = "fill") +
-  ggtitle("Loan's Date impact on loan's success")
+  ggtitle("Month Impact on Loan's success")
 
 ####
 
-# Jitter Plot for Account Frquency Access
+# Jitter Plot for Account Frequency Access
 ggplot(df, aes(status, account_freq_access)) +
   geom_jitter(aes(color = account_freq_access), size = 0.5)
 
@@ -248,4 +248,100 @@ library(corrplot)
 # correlation Matrix but make it more aesthetically appealing
 corrplot(cormat, type = "upper", order = "hclust", 
          tl.col = "black", tl.srt = 45)
+
+#######################
+
+
+df_district <- df_new[,c("region","nb_inhabitants", "nb_cities", "ratio_urban_inhabitants", "average_salary", "nb_enterpreneurs_per1000", "unemployment95",  "crime95_S", "crime96_S")]
+
+cormat <- round(cor(df_district, method = "pearson"),2)
+corrplot(cormat, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+# 2. Reorder the correlation matrix by 
+# Hierarchical clustering
+hc <- hclust(as.dist(1-cormat)/2)
+cormat.ord <- cormat[hc$order, hc$order]
+# 3. Get the upper triangle
+cormat.ord[lower.tri(cormat.ord)]<- NA
+# 4. Melt the correlation matrix
+require(reshape2)
+melted_cormat <- melt(cormat.ord, na.rm = TRUE)
+# Create the heatmap
+ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab",
+                       name="Pearson\nCorrelation") + # Change gradient color
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed() + #
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+###########################################
+
+df_loans <- df_new[,c("gender","age", "average_salary", "loan_amount", "loan_duration", "payments", "status", "loan_date_year", "loan_date_monthNB")]
+
+cormat <- round(cor(df_loans, method = "pearson"),2)
+corrplot(cormat, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+# 2. Reorder the correlation matrix by 
+# Hierarchical clustering
+hc <- hclust(as.dist(1-cormat)/2)
+cormat.ord <- cormat[hc$order, hc$order]
+# 3. Get the upper triangle
+cormat.ord[lower.tri(cormat.ord)]<- NA
+# 4. Melt the correlation matrix
+require(reshape2)
+melted_cormat <- melt(cormat.ord, na.rm = TRUE)
+# Create the heatmap
+ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab",
+                       name="Pearson\nCorrelation") + # Change gradient color
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed() + #
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- dm-proj1/stats.r
+++ dm-proj1/stats.r
@@ -5,3 +5,124 @@ print("Successful loans")
 print(summarise(loans_successful, nbWomen = sum(gender == "F"), nbMen = sum(gender == "M"), avgAge = mean(age), 
                 avgSalary = mean(average_salary), avgUnemploymentRate95 = mean(unemployment95, na.rm=TRUE),
                 avgUnemploymentRate96 = mean(unemployment96), avgCrimeRate95 = mean(crime95, na.rm=TRUE), avgCrimeRate96 = mean(crime96)))
+
+###############################################
+
+
+library(dplyr)
+library(readr)
+
+# Reading all files
+clients <- read_delim("ficheiros_competicao/client.csv", delim = ";")
+loans <- read_delim("ficheiros_competicao/loan_train.csv", delim = ";")
+accounts <- read_delim("ficheiros_competicao/account.csv", delim = ";")
+cards <- read_delim("ficheiros_competicao/card_train.csv", delim = ";")
+districts <- read_delim("ficheiros_competicao/district.csv", delim = ";")
+disp <- read_delim("ficheiros_competicao/disp.csv", delim = ";")
+transactions <- read_delim("ficheiros_competicao/trans_train.csv", delim = ";")
+
+
+# Cleaning District Dataframe
+# Fixing Column Names
+names(districts)[names(districts) == 'code '] <- 'code'
+names(districts)[names(districts) == 'name '] <- 'name'
+names(districts)[names(districts) == 'no. of inhabitants'] <- 'nb_hab'
+names(districts)[names(districts) == 'no. of cities '] <- 'nb_cities'
+names(districts)[names(districts) == 'ratio of urban inhabitants '] <- 'ratio_urban_hab'
+names(districts)[names(districts) == 'average salary '] <- 'avg_salary'
+names(districts)[names(districts) == 'unemploymant rate \'95 ' ] <- 'unemployment95'
+names(districts)[names(districts) == 'unemploymant rate \'96 ' ] <- 'unemployment96'
+names(districts)[names(districts) == 'no. of enterpreneurs per 1000 inhabitants '] <- 'nb_enterpreneurs_per1000'
+names(districts)[names(districts) == 'no. of commited crimes \'95 ' ] <- 'crime95'
+names(districts)[names(districts) == 'no. of commited crimes \'96 ' ] <- 'crime96'
+
+# Unemployment and Crime Rate from '95 are stored as Characters instead of Numeric values
+df <- districts[,c("unemployment95","crime95")]
+df[sapply(df, is.character)] <-lapply(df[sapply(df, is.character)], as.numeric)
+districts$unemployment95 <- df$unemployment95
+districts$crime95 <- df$crime95
+rm(df)
+
+# Standardize Crime Values (normalization)) 
+districts$crime95_S <- (districts$crime95 / districts$nb_hab)
+districts$crime96_S <- (districts$crime96 / districts$nb_hab)
+
+sum(duplicated(disp$client_id))
+
+##################################################################################
+
+#unique(df)
+summary(df)
+#rm(group_by_region2)
+
+group_by_region <-
+  aggregate(x = df$client_id,          # Specify data column
+            by = list(df$region),      # Specify group indicator
+            FUN = function(x) length(unique(x))) #Desired function
+group_by_region$Percentage <- round(group_by_region$x*100/sum(group_by_region$x), digits=2)
+
+central_Bohemia <- dplyr::filter(districts, region == "central Bohemia")
+south_Bohemia <- dplyr::filter(districts, region == "south Bohemia")
+north_Bohemia <- dplyr::filter(districts, region == "north Bohemia")
+west_Bohemia <- dplyr::filter(districts, region == "west Bohemia")
+east_Bohemia <- dplyr::filter(districts, region == "east Bohemia")
+north_Moravia <- dplyr::filter(districts, region == "north Moravia")
+south_Moravia <- dplyr::filter(districts, region == "south Moravia")
+prague <- dplyr::filter(districts, region == "Prague")
+
+
+print("Central Bohemia")
+print(summarise(prague, nbInhabitants = sum(nb_hab), nbCities = sum(nb_cities), avgSalary = mean(avg_salary), 
+                avgRatioUrbanHab = mean(ratio_urban_hab), avgEntrepeneurs = mean(nb_enterpreneurs_per1000), avgUnemploymentRate95 = mean(unemployment95, na.rm=TRUE),
+                avgUnemploymentRate96 = mean(unemployment96), avgCrimeRate95 = mean(crime95_Standardized, na.rm=TRUE), avgCrimeRate96 = mean(crime96_Standardized)))
+
+##################################################################################
+
+df_acc <- accounts
+df_acc$region <- districts$region[match(df_acc$district_id, districts$code)]
+
+print(summarise(df_acc, centralBohemia = sum(region == "central Bohemia"), eastBohemia = sum(region == "east Bohemia"),
+                northlBohemia = sum(region == "north Bohemia"), southBohemia = sum(region == "south Bohemia"),
+                westBohemia = sum(region == "west Bohemia"), northMoravia = sum(region == "north Moravia"),
+                southMoravia = sum(region == "south Moravia"), prague = sum(region == "Prague")
+                ))
+
+#rm(df_acc)
+#################################################################################
+
+df_acc$status <- loans$status[match(df_acc$account_id, loans$account_id)]
+
+df_acc <- subset(df_acc, select = -account_id )
+df_acc <- subset(df_acc, select = -district_id )
+df_acc <- subset(df_acc, select = -frequency )
+df_acc <- subset(df_acc, select = -date )
+df_acc <- subset(df_acc, select = -account_creation_date )
+
+df_acc <- na.omit(df_acc)
+
+print(summarise(df_acc, centralBohemia = sum(region == "central Bohemia"), eastBohemia = sum(region == "east Bohemia"),
+                northlBohemia = sum(region == "north Bohemia"), southBohemia = sum(region == "south Bohemia"),
+                westBohemia = sum(region == "west Bohemia"), northMoravia = sum(region == "north Moravia"),
+                southMoravia = sum(region == "south Moravia"), prague = sum(region == "Prague")
+))
+
+
+# Region's Impact on loan's success 
+ggplot(df_acc, aes(x = region, group = status, fill = status)) +
+  geom_bar(position = "fill") +
+  ggtitle("Region impact on loan's success")
+
+round(554*100/4500, digits=2)
+
+summary(loans)
+
+
+
+
+
+
+
+
+
+
+

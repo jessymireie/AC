b10653de8c184d61dc735b8c985c1d18fc559913
alexmsf <xana.ferreira2000@hotmail.com>
2021-12-02 13:24:29 +0000

    confusion matrix for accuracy in random forest prediction

--- dm-proj1/random_forest.r
+++ dm-proj1/random_forest.r
@@ -14,7 +14,7 @@ test <- read.csv('final_test.csv',sep = ',', header=TRUE)
 # Converting 'status' to a factor
 train$status <- factor(train$status)
 # Set a random seed
-set.seed(51)
+set.seed(100) # if this value is not set, it will default to random, and results will vary with every run
 # Training using 'random forest' algorithm
 
 # status is a function of the variables we decided to include
@@ -41,4 +41,30 @@ export <- results[,c("loan_id", "status_neg_prob")]
 names(export)[names(export) == 'loan_id' ] <- 'Id'
 names(export)[names(export) == 'status_neg_prob' ] <- 'Predicted'
 
-write.csv(export,"rf_first.csv", row.names = FALSE)
+write.csv(export,"results/rf_first_2.csv", row.names = FALSE)
+
+##### GETTING MODEL ACCURACY
+
+# load the libraries
+library(klaR)
+# load the iris dataset
+
+# define an 80%/20% train/test split of the dataset
+split=0.80
+trainIndex <- createDataPartition(train$status, p=split, list=FALSE)
+data_train <- train[ trainIndex,]
+data_test <- train[-trainIndex,]
+# train a model
+model <- train(status ~ payments_loan + duration_loan + num_credit + avg_credit + max_credit + min_credit + median_credit + iqr_credit
+               + num_withdrawal + avg_withdrawal + max_withdrawal + min_withdrawal + median_withdrawal + iqr_withdrawal
+               + avg_amount + median_amount + iqr_amount + max_balance + min_balance + avg_balance + median_balance + iqr_balance + frequency_num
+               + avg_monthly_balance + time_bf_loan + age_at_loan + card_num,
+               data = data_train, 
+               method = 'rf', 
+               trControl = trainControl(method = 'cv', number = 5))
+# make predictions
+x_test <- subset(data_test, select = -status )
+y_test <- data_test$status
+predictions <- predict(model, x_test)
+# summarize results
+confusionMatrix(predictions, y_test)
